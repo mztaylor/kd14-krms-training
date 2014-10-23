1489f6bfe1299f97c639b00d8b0be4e16f65265f
stonesteps <dseibert@vivantech.com>
2014-10-23 15:11:57 -0700

    added campus term

--- src/main/config/rules/db/mysql/setup/005-term-specification.sql
+++ src/main/config/rules/db/mysql/setup/005-term-specification.sql
@@ -16,3 +16,7 @@
 insert into `KRMS_TERM_SPEC_T` (`TERM_SPEC_ID`,`NMSPC_CD`,`NM`,`TYP`,`ACTV`,`VER_NBR`,`DESC_TXT`) values ('KRTRN-TS-COLLEGE','KRA-TRNG','college','java.lang.String','Y',1,'Selected College');
 insert into `KRMS_TERM_T` (`TERM_ID`,`TERM_SPEC_ID`,`VER_NBR`,`DESC_TXT`) values ('KRTRN-T-COLLEGE','KRTRN-TS-COLLEGE',1,'College');
 insert into `KRMS_CNTXT_VLD_TERM_SPEC_T` (`CNTXT_TERM_SPEC_PREREQ_ID`,`CNTXT_ID`,`TERM_SPEC_ID`,`PREREQ`) values ('KRTRN-VLD-T-001','KRTRN-CONTEXT','KRTRN-TS-COLLEGE','N');
+
+insert into `KRMS_TERM_SPEC_T` (`TERM_SPEC_ID`,`NMSPC_CD`,`NM`,`TYP`,`ACTV`,`VER_NBR`,`DESC_TXT`) values ('KRTRN-TS-CAMPUS','KRA-TRNG','campus','java.lang.String','Y',1,'Selected Campus');
+insert into `KRMS_TERM_T` (`TERM_ID`,`TERM_SPEC_ID`,`VER_NBR`,`DESC_TXT`) values ('KRTRN-T-CAMPUS','KRTRN-TS-CAMPUS',1,'Campus');
+insert into `KRMS_CNTXT_VLD_TERM_SPEC_T` (`CNTXT_TERM_SPEC_PREREQ_ID`,`CNTXT_ID`,`TERM_SPEC_ID`,`PREREQ`) values ('KRTRN-VLD-T-002','KRTRN-CONTEXT','KRTRN-TS-CAMPUS','N');
